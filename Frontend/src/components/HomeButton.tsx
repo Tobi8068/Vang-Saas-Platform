d684b5ae01da8b513253ef8fbeefe71b18788a45
tobi8068 <hana.dev22@gmail.com>
2024-08-27 01:19:55 +1000

    [feat]: updated some issues: menu button added and searchbar updated

--- Frontend/src/components/HomeButton.tsx
+++ Frontend/src/components/HomeButton.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import HomeIcon from "assets/img/Home.png";
 
 function HomeButton() {
     return (
-        <div className="bg-[#E1FF67] flex items-center px-4 rounded-[15px] shadow-md">
+        <Link to='/' className="bg-[#E1FF67] flex items-center px-4 rounded-[15px] shadow-md">
             <img src={HomeIcon} alt="Home Icon" className="h-[30px] w-[30px]"></img>
-        </div>
+        </Link>
     )
 }
 
--- Frontend/src/layout/discover/CustomCompanyStack.tsx
+++ Frontend/src/layout/discover/CustomCompanyStack.tsx
@@ -1,85 +1,79 @@
 import Card from "components/Card";
 
-function CustomCompanyStack () { 
+function CustomCompanyStack() {
 
     const cardData = [
         {
             title: "SalesForce",
             rating: 4.8,
-            reviews: "25,652",
-            description: `The longest word in any of the major English language dictionaries 
-            is pneumon ultramicroscopi csilicovol canoconiosis, 
-            a word that refers to a lung disease contracted from
-            the inhalation of very fine silica particles, specifically
-            from a volcano; medically, it is the same as silicosis.`
+            reviews: 25652,
+            description: `Salesforce brings together all your data, from any source. 
+            Einstein 1, our complete suite of products, unites your sales, service, marketing, 
+            commerce, and IT teams with a single, shared view of customer information. With artificial 
+            intelligence integrated across all products, Salesforce helps everyone in your company work 
+            more productively and better deliver the personalized experiences customers love.`
         },
         {
             title: "HubSpot",
             rating: 4.5,
-            reviews: "15,432",
+            reviews: 15432,
             description: `HubSpot is a developer and marketer of software products for inbound marketing, sales, and customer service.`
         },
         {
             title: "Zoho",
             rating: 4.6,
-            reviews: "12,345",
+            reviews: 12345,
             description: `Zoho offers a comprehensive suite of online productivity tools and SaaS applications.`
         },
         {
             title: "SalesForce",
             rating: 4.8,
-            reviews: "25,652",
-            description: `The longest word in any of the major English language dictionaries 
-            is pneumon ultramicroscopi csilicovol canoconiosis, 
-            a word that refers to a lung disease contracted from
-            the inhalation of very fine silica particles, specifically
-            from a volcano; medically, it is the same as silicosis.`
+            reviews: 25652,
+            description: `Salesforce brings together all your data, from any source. 
+            Einstein 1, our complete suite of products, unites your sales, service, marketing, 
+            commerce, and IT teams with a single, shared view of customer information. With artificial 
+            intelligence integrated across all products, Salesforce helps everyone in your company work 
+            more productively and better deliver the personalized experiences customers love.`
         },
         {
             title: "HubSpot",
             rating: 4.5,
-            reviews: "15,432",
+            reviews: 15432,
             description: `HubSpot is a developer and marketer of software products for inbound marketing, sales, and customer service.`
         },
         {
             title: "Zoho",
             rating: 4.6,
-            reviews: "12,345",
+            reviews: 12345,
             description: `Zoho offers a comprehensive suite of online productivity tools and SaaS applications.`
         },
         {
             title: "SalesForce",
             rating: 4.8,
-            reviews: "25,652",
-            description: `The longest word in any of the major English language dictionaries 
-            is pneumon ultramicroscopi csilicovol canoconiosis, 
-            a word that refers to a lung disease contracted from
-            the inhalation of very fine silica particles, specifically
-            from a volcano; medically, it is the same as silicosis.`
+            reviews: 25652,
+            description: `Salesforce brings together all your data, from any source. 
+            Einstein 1, our complete suite of products, unites your sales, service, marketing, 
+            commerce, and IT teams with a single, shared view of customer information. With artificial 
+            intelligence integrated across all products, Salesforce helps everyone in your company work 
+            more productively and better deliver the personalized experiences customers love.`
         },
         {
             title: "HubSpot",
             rating: 4.5,
-            reviews: "15,432",
+            reviews: 15432,
             description: `HubSpot is a developer and marketer of software products for inbound marketing, sales, and customer service.`
         },
         {
             title: "Zoho",
             rating: 4.6,
-            reviews: "12,345",
+            reviews: 12345,
             description: `Zoho offers a comprehensive suite of online productivity tools and SaaS applications.`
         },
         // Add more card data as needed
     ];
 
-    const text = `The longest word in any of the major English language dictionaries 
-    is pneumono ultramicroscopi csilicovol canoconiosis, 
-    a word that refers to a lung disease contracted from
-     the inhalation of very fine silica particles, specifically
-      from a volcano; medically, it is the same as silicosis. `;
-
     const handleClick = () => {
-        
+
     }
 
     return (
