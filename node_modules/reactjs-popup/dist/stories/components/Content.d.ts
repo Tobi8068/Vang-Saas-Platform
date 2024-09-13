30210edc9d8d24f2316759654b726a7a58207e69
tobi8068 <hana.dev22@gmail.com>
2024-09-14 04:03:31 +1000

    Revert "[update]: removed eslint"
    This reverts commit c1a67351deac54a300d334447273bc38712726fd.

--- node_modules/reactjs-popup/dist/stories/components/Content.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-declare type Props = {
-    children: ReactNode;
-    close?: () => void;
-    title?: string;
-};
-export declare const Content: ({ children, close, title, }: Props) => JSX.Element;
-export {};
